345fa359c42497bcb47d01740e8e429a0c07c952
1010K <106136796+1010K@users.noreply.github.com>
2022-06-02 19:58:33 +0900

    Update W7_Profile.dart

--- W7_Profile.dart
+++ W7_Profile.dart
@@ -2,31 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class W7_Profile extends StatelessWidget {
+    //暫定的にここに適当なリストを作って表示させています。
+    //このリストにはユーザの連絡先が格納される予定です。
     final List<String> entries = <String>['A', 'B', 'C'];
 
     @override
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
-                title: Text('課題名'),//飛ぶ前のページと同じ課題名(書かなくてもいいかも)
+                //飛ぶ前のページと同じ課題名(書かなくてもいいかも)
+                title: Text('課題名'),
             ),
             body: Column(
             children: <Widget>[
+                //学籍番号(ユーザ名)表示
                 Text(
                     'AA00000', 
-                    style: TextStyle(fontWeight: FontWeight.bold, height: 1.5, fontSize: 40),
-                ),            
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        height: 1.5, 
+                        fontSize: 40
+                    ),
+                ), 
+                //学籍番号の下線   
                 const Divider(
                     height: 10,
                     thickness: 3,
                     indent: 20,
                     endIndent: 20,
                     color: Colors.green,
-                ),
+                ),  
                 Container(
                 height: 125,
                 padding: const EdgeInsets.all(4),
-                // 各アイテムの間にスペースなどを挟みたい場合
+                //連絡先リスト
                 child: entries.length > 0
                     ? ListView.separated(
                     itemCount: entries.length,
@@ -39,6 +48,7 @@ class W7_Profile extends StatelessWidget {
                         return const Divider();
                     },
                     )
+                    //リストが空の場合は No items と表示
                     : const Center(child: Text('No items')),
                 ),
             ],
