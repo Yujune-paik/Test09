023ef26e778ccf5a744939796db19a6c065d86c4
Yujune-paik <102679983+Yujune-paik@users.noreply.github.com>
2022-06-07 13:50:12 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _searchBoolean = false;
+  List<int> _searchIndexList = [];
+
+  List<String> _list = ['高度情報処理演習1A','情報工学通論'];
+
+  Widget _searchTextField() {
+    return TextField(
+      onChanged: (String s) {
+        setState(() {
+          _searchIndexList = [];
+          for (int i = 0; i < _list.length; i++) {
+            if (_list[i].contains(s)) {
+              _searchIndexList.add(i);
+            }
+          }
+        });
+      },
+      autofocus: true,
+      cursorColor: Colors.white,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),
+      textInputAction: TextInputAction.search,
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)
+        ),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)
+        ),
+        hintText: 'Search',
+        hintStyle: TextStyle(
+          color: Colors.white60,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _searchListView() {
+    return ListView.builder(
+        itemCount: _searchIndexList.length,
+        itemBuilder: (context, index) {
+          index = _searchIndexList[index];
+          return Card(
+              child: ListTile(
+                  title: Text(_list[index])
+              )
+          );
+        }
+    );
+  }
+
+  Widget _defaultListView() {
+    return ListView.builder(
+        itemCount: _list.length,
+        itemBuilder: (context, index) {
+          return Card(
+              child: ListTile(
+                  title: Text(_list[index])
+              )
+          );
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: !_searchBoolean ? Text(widget.title) : _searchTextField(),
+            actions: !_searchBoolean
+                ? [
+              IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    setState(() {
+                      _searchBoolean = true;
+                      _searchIndexList = [];
+                    });
+                  })
+            ]
+                : [
+              IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    setState(() {
+                      _searchBoolean = false;
+                    });
+                  }
+              )
+            ]
+        ),
+        body: !_searchBoolean ? _defaultListView() : _searchListView()
+    );
+  }
+}
