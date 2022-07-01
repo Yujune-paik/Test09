740ec4f57c1bc8f690eb86a10d1b869bb4ced2ca
takerumaguro <106137505+takerumaguro@users.noreply.github.com>
2022-07-01 18:27:07 +0900

    Add files via upload

---
+++ lib/Login.dart
@@ -0,0 +1,78 @@
+/******************************************************************
+ ***  File Name		: SubjectServer.dart
+ ***  Version		: V1.0
+ ***  Designer		: 中田 裕紀
+ ***  Date			: 2022.07.01
+ ***  Purpose       	: 科目情報をデータベースから引き出す
+ ***
+ *******************************************************************/
+/*
+*** Revision :
+*** V1.0 : 中田裕紀, 2022.07.01
+*/
+
+import 'dart:async';
+import 'package:http/http.dart';
+import 'package:mysql1/mysql1.dart';
+import 'package:mysql_utils/mysql_utils.dart';
+
+class Login {
+  //科目情報を引き出す
+  Future<int> check(String studentId, String inputPassWord) async {
+    // データベースと接続
+    var db = MysqlUtils(
+        settings: {
+          'host': '160.16.141.77',
+          'port': 50900,
+          'user': 'app09',
+          'password': 'pracb2022',
+          'db': 'App_db',
+          'maxConnections': 10,
+          'secure': false,
+          'prefix': '',
+          'pool': true,
+          'collation': 'utf8mb4_general_ci',
+        },
+        errorLog: (error) {
+          print(error);
+        },
+        sqlLog: (sql) {
+          print(sql);
+        },
+        connectInit: (db1) async {
+          print('whenComplete');
+        });
+
+    //select文
+    var row = await db.query(
+        'SELECT PassWord FROM App_db.StudentInfo where StudentNum = ("$studentId")');
+
+    String PassWord = "";
+
+    // PassWordを取得する
+    for (var pass in row.rowsAssoc) {
+      PassWord = pass.colAt(0);
+      break;
+    }
+/*
+    if(PassWord == ""){
+      return Future.value(0);
+    }
+*/
+// データベースを閉じる
+    await db.close();
+    print(inputPassWord);
+    print(PassWord);
+    if (inputPassWord == PassWord)
+      return Future.value(1);
+    else
+      return Future.value(0);
+  }
+}
+
+// // テスト用のmain関数
+ void main() async {
+     Future<int> _futureOfList = Login().check("al20001", "hage");
+     int result = await _futureOfList;
+    print(result);
+ }
---
+++ lib/W2-1_MyHomePage.dart
@@ -0,0 +1,251 @@
+import 'package:flutter/material.dart';
+//From. Added 小筆赳 2022.6.9
+import 'W2-2_MyHomePage.dart';
+import 'W3_Search.dart';
+import 'W4_Completed.dart';
+import 'W5_AddTask.dart';
+import 'W6-1_MyPage.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/cupertino.dart';
+import 'Task_database.dart';
+import 'Task_database_model.dart';
+//To. Added 小筆赳 2022.6.9
+
+
+//ホーム画面(未提出)
+
+class W2_1_MyHomePage extends StatefulWidget {
+  const W2_1_MyHomePage({Key?key}) : super(key: key);
+
+  @override
+  _W2_1_MyHomePageState createState() => _W2_1_MyHomePageState();
+}
+
+class _W2_1_MyHomePageState extends State<W2_1_MyHomePage>{
+/*
+  List<String>result = [];//関数から持ってきた課題リスト
+  result = read_AllTask(tasks.taskname);
+*/
+
+  List<Task>tasks = [];
+  bool isLoading = false;
+  final TextEditingController _categoryNameController =
+  new TextEditingController(text: '');
+
+  @override
+  void initState() {
+    super.initState();
+    loadTasks();
+  }
+
+
+
+  @override
+  void dispose() {
+    TaskDatabase.instance.closeDatabase();
+    super.dispose();
+  }
+
+  Future loadTasks() async {
+    setState(() => isLoading = true);
+    await TaskDatabase.instance.deleteExpiredTask();
+    tasks = await TaskDatabase.instance.readAllTask();
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green, //アプリバーの背景色
+        actions: <Widget>[
+      IconButton(
+      icon: const Icon(Icons.account_circle,),
+      onPressed: () {
+        //From. Added 小筆赳 2022.6.9
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => W6_Mypage()),
+        );//To.Changed　小筆赳 2022.6.9
+      },
+    ),
+    ],
+      ),
+        body:  Container(
+          width: double.infinity,
+          padding:const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+
+
+              //課題検索ページへ
+
+              ButtonBar(
+                children: [
+                  TextButton(
+                    child: Text('課題を検索する'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => W3_Search(title: '',/*read_AllTask*/)),
+                      );
+                    },
+                  ),
+                ],
+              ),
+
+
+              //課題の提出状態
+              ButtonBar(
+                //child: Row(
+                //alignment: Alignment.topRight,
+                children: [
+                  ElevatedButton(
+                    child: const Text('未提出'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      onPrimary: Colors.white,
+                      shape: const StadiumBorder(),
+
+                    ),
+                    onPressed: () {
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text('提出済'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.green,
+                      shape: const StadiumBorder(),
+                    ),
+                    onPressed: () {
+                      //From. Added 小筆赳 2022.6.9
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => W2_2MyHomePage()),
+                      );//To. Added 小筆赳 2022.6.9
+                    },
+                  ),
+                ],
+                //),
+              ),
+
+              //課題リスト
+              SizedBox(
+                height: 500,
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index){
+                    final task = tasks[index];
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  task.taskname,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                  ),
+                          ),
+                          Row(
+                            children: [
+                              const Text('期限：'),
+                              Text(DateFormat('yyyy/MM/dd HH:mm')
+                                  .format(task.deadline)),
+                            ],
+                          ),
+                          ],
+                        ),
+                                IconButton(
+                                  alignment: Alignment.center,
+                                  icon: const Icon(
+                                    Icons.check_circle_outline,
+                                    color: Colors.green,
+                                  ),
+                                  onPressed: () async {
+                                    await TaskDatabase.instance.completeTask(task.id!);
+                                  },
+                                ),
+                            /*
+                            onTap: (){
+                            //add_Whocompleted
+                              //delete
+                            },*/
+                              ],
+                          ),
+
+                        ),
+
+
+                    );
+                  },
+                ),
+              ),
+
+
+                      /*
+                      title: Text(kadai_mi[index]['text']),
+                      subtitle: Text(kadai_mi[index]['date']),
+
+                      //チェックボタン
+                      trailing: IconButton(
+                        iconSize: 30,
+                        icon: const Icon(Icons.check_circle_outline,),
+                        onPressed: () {},
+                      ),
+                      //dense: true,
+
+                      //クリックされた時の処理（提出済みに移動）//課題完了者一覧に
+                      onTap: (){
+                        //From. Added 小筆赳 2022.6.9
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => W4_Completed()),
+                        );//To. Added 小筆赳 2022.6.9
+                      },*/
+
+
+              /*const ButtonBar(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.add_circle),
+                    onPressed: () {},
+                    color: Colors.green,
+                    iconSize: 60,
+                  ),
+                ],
+              ),*/
+
+            ],
+          ),
+
+        ),
+        //課題追加ボタン
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //From. Added 小筆赳 2022.6.9
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => W5_AddTask(),
+            ),
+            );//To. Added 小筆赳 2022.6.9
+          },
+          child: const Icon(Icons.add),
+          backgroundColor: Colors.green,
+
+        ),
+      );
+  }
+}
