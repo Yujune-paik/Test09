cf91528656281a194c9c553a813d6988efc82ae8
KuritaHaruki <106136720+KuritaHaruki@users.noreply.github.com>
2022-07-04 01:25:33 +0900

    Add files via upload

--- lib/W2-1_MyHomePage.dart
+++ lib/W2-1_MyHomePage.dart
@@ -57,6 +57,7 @@ class _W2_1_MyHomePageState extends State<W2_1_MyHomePage>{
   @override
   void initState() {
     super.initState();
+    disPush();
     loadTasks();
   }
 
@@ -82,143 +83,153 @@ class _W2_1_MyHomePageState extends State<W2_1_MyHomePage>{
       appBar: AppBar(
         backgroundColor: Colors.green, //アプリバーの背景色
         actions: <Widget>[
-      IconButton(
-      icon: const Icon(Icons.account_circle,),
-      onPressed: () {
-        //From. Added 小筆赳 2022.6.9
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => W6_Mypage()),
-        );//To.Changed　小筆赳 2022.6.9
-      },
-    ),
-    ],
+          IconButton(
+            icon: const Icon(Icons.account_circle,),
+            onPressed: () {
+              //From. Added 小筆赳 2022.6.9
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => W6_Mypage()),
+              );//To.Changed　小筆赳 2022.6.9
+            },
+          ),
+        ],
       ),
-        body:  Container(
-          width: double.infinity,
-          padding:const EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
+      body:  Container(
+        width: double.infinity,
+        padding:const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
 
 
-              //課題検索ページへ
+            //課題検索ページへ
 
-              ButtonBar(
-                children: [
-                  TextButton(
-                    child: Text('課題を検索する'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => W3_Search(title: '',/*read_AllTask*/)),
-                      );
-                    },
+            ButtonBar(
+              children: [
+                TextButton(
+                  child: Text('課題を検索する'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => W3_Search(title: '',/*read_AllTask*/)),
+                    );
+                  },
+                ),
+              ],
+            ),
+
+
+            //課題の提出状態
+            ButtonBar(
+              //child: Row(
+              //alignment: Alignment.topRight,
+              children: [
+                ElevatedButton(
+                  child: const Text('未提出'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    onPrimary: Colors.white,
+                    shape: const StadiumBorder(),
+
                   ),
-                ],
-              ),
-
-
-              //課題の提出状態
-              ButtonBar(
-                //child: Row(
-                //alignment: Alignment.topRight,
-                children: [
-                  ElevatedButton(
-                    child: const Text('未提出'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                      onPrimary: Colors.white,
-                      shape: const StadiumBorder(),
-
-                    ),
-                    onPressed: () {
-                    },
+                  onPressed: () {
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('提出済'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.green,
+                    shape: const StadiumBorder(),
                   ),
-                  ElevatedButton(
-                    child: const Text('提出済'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      onPrimary: Colors.green,
-                      shape: const StadiumBorder(),
-                    ),
-                    onPressed: () {
-                      //From. Added 小筆赳 2022.6.9
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => W2_2MyHomePage()),
-                      );//To. Added 小筆赳 2022.6.9
-                    },
-                  ),
-                ],
-                //),
-              ),
+                  onPressed: () {
+                    //From. Added 小筆赳 2022.6.9
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => W2_2MyHomePage()),
+                    );//To. Added 小筆赳 2022.6.9
+                  },
+                ),
+              ],
+              //),
+            ),
 
-              //課題リスト
-              SizedBox(
-                height: 500,
-                child: ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index){
-                    final task = tasks[index];
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  task.taskname,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 24,
-                                  ),
-                          ),
-                            Row(
-                              children: [
-                                const Text('期限：'),
-                                Text(DateFormat('yyyy/MM/dd HH:mm')
-                                    .format(task.deadline)),
-                              ],
-                            ),
-                          ],
-                        ),
-                                IconButton(
-                                  alignment: Alignment.center,
-                                  icon: const Icon(
-                                    Icons.check_circle_outline,
-                                    color: Colors.green,
-                                  ),
-                                  onPressed: () async {
-                                    await TaskDatabase.instance.completeTask(task.id!);
-                                    if(task.isPrivate != '-1') {
-                                      TaskServer().addWhoCompleted(task.isPrivate,studentNum);
-                                    }
-                                  },
+            //課題リスト
+            SizedBox(
+              height: 500,
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index){
+                  final task = tasks[index];
+                  return Card(
+
+                    child:InkWell(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                task.taskname,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
                                 ),
-                            /*
+                              ),
+                              Row(
+                                children: [
+                                  const Text('期限：'),
+                                  Text(DateFormat('yyyy/MM/dd HH:mm')
+                                      .format(task.deadline)),
+                                ],
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            alignment: Alignment.center,
+                            icon: const Icon(
+                              Icons.check_circle_outline,
+                              color: Colors.green,
+                            ),
+                            onPressed: () async {
+                              await TaskDatabase.instance.completeTask(task.id!);
+                              if(task.isPrivate != '-1') {
+                                TaskServer().addWhoCompleted(task.isPrivate,studentNum);
+                              }
+                            },
+                          ),
+                          /*
                             onTap: (){
 
 
                             },*/
-                              ],
+                        ],
+                      ),
+
+                    ),
+                      onTap: () async {
+                        await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => W4_Completed(),
                           ),
+                        );
+                        loadTasks();
+                      },
 
-                        ),
-
-
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
+            ),
 
 
-                      /*
+            /*
                       title: Text(kadai_mi[index]['text']),
                       subtitle: Text(kadai_mi[index]['date']),
 
@@ -241,7 +252,7 @@ class _W2_1_MyHomePageState extends State<W2_1_MyHomePage>{
                       },*/
 
 
-              /*const ButtonBar(
+            /*const ButtonBar(
                 children: [
                   IconButton(
                     icon: const Icon(Icons.add_circle),
@@ -252,26 +263,26 @@ class _W2_1_MyHomePageState extends State<W2_1_MyHomePage>{
                 ],
               ),*/
 
-            ],
-          ),
-
+          ],
         ),
-        //課題追加ボタン
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            //From. Added 小筆赳 2022.6.9
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => W5_AddTask(),
+
+      ),
+      //課題追加ボタン
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //From. Added 小筆赳 2022.6.9
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => W5_AddTask(),
             ),
-            );//To. Added 小筆赳 2022.6.9
-          },
-          child: const Icon(Icons.add),
-          backgroundColor: Colors.green,
+          );//To. Added 小筆赳 2022.6.9
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.green,
 
-        ),
-      );
+      ),
+    );
   }
 }
 
