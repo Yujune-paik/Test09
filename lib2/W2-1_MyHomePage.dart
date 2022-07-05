d49daf34b713e62814c9c8d0e5f84a27f829c8e2
1010K <106136796+1010K@users.noreply.github.com>
2022-07-05 15:35:48 +0900

    Update W2-1_MyHomePage.dart

--- lib2/W2-1_MyHomePage.dart
+++ lib2/W2-1_MyHomePage.dart
@@ -181,7 +181,7 @@ class _W2_1_MyHomePageState extends State<W2_1_MyHomePage>{
                   ),
                   onPressed: () {
                     //From. Added 小筆赳 2022.6.9
-                    Navigator.push(
+                    Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
                           builder: (context) => W2_2MyHomePage()),
